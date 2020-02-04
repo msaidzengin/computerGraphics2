4322e2139254bf5c086e80657323e111c462e0a7
msaidzengin <m.zengin@etu.edu.tr>
2020-02-04 12:30:01 +0300

    sphere

--- problem3.cpp
+++ problem3.cpp
@@ -1,62 +1,53 @@
 #include <GL\glut.h>
 
 GLfloat xRotated, yRotated, zRotated;
-GLdouble radius = 1;
+GLdouble radius = 2;
 
 void reshape(int x, int y) {
     if (y == 0 || x == 0) return;
     glMatrixMode(GL_PROJECTION);
     glLoadIdentity();
-    gluPerspective(39.0, (GLdouble)x / (GLdouble)y, 0.6, 21.0);
+    gluPerspective(100, (GLdouble)x / (GLdouble)y, 3, 50);
     glMatrixMode(GL_MODELVIEW);
-    glViewport(0, 0, x, y);  //Use the whole window for rendering
+    glViewport(0, 0, x, y);
 }
 
 void display(void) {
 
     glMatrixMode(GL_MODELVIEW);
-    // clear the drawing buffer.
     glClear(GL_COLOR_BUFFER_BIT);
-    // clear the identity matrix.
     glLoadIdentity();
-    // traslate the draw by z = -4.0
-    // Note this when you decrease z like -8.0 the drawing will looks far , or smaller.
+
     glTranslatef(0.0, 0.0, -5.0);
-    // Red color used to draw.
     glColor3f(0.9, 0.3, 0.2);
-    // changing in transformation matrix.
-    // rotation about X axis
+
     glRotatef(xRotated, 1.0, 0.0, 0.0);
-    // rotation about Y axis
     glRotatef(yRotated, 0.0, 1.0, 0.0);
-    // rotation about Z axis
     glRotatef(zRotated, 0.0, 0.0, 1.0);
-    // scaling transfomation 
+
     glScalef(1.0, 1, 1.0);
-    // built-in (glut library) function , draw you a sphere.
     glutWireSphere(radius, 20, 20);
-    // Flush buffers to screen
+
+    glutWireSphere(10, 100, 20);
 
     glFlush();
-    // sawp buffers called because we are using double buffering 
-   // glutSwapBuffers();
 }
 
 void idle(void) {
 
     xRotated += 0.01;
-    //yRotated += 0.01;
+    yRotated += 0.01;
     zRotated += 0.01;
     display();
 }
 
 int main(int argc, char** argv) {
     glutInit(&argc, argv);
-    glutInitWindowSize(350, 350);
+    glutInitWindowSize(500, 500);
     glutCreateWindow("Solid Sphere");
-    xRotated = yRotated = zRotated = 30.0;
-    xRotated = 43;
-    yRotated = 50;
+    xRotated = 0;
+    yRotated = 0;
+    zRotated = 0;
 
     glutDisplayFunc(display);
     glutReshapeFunc(reshape);
