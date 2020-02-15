f49c9397de49037454b4c617872538016741f7a8
msaidzengin <m.zengin@etu.edu.tr>
2020-02-15 17:02:10 +0300

    wire sphere added

--- problem3.cpp
+++ problem3.cpp
@@ -1,59 +1,147 @@
-#include <GL\glut.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <math.h>
+#include <GL/glut.h>
+#include <iostream>
+#include <cmath>
+#include <iostream>
+#include <cmath>
+using namespace std;
 
-GLfloat rotate = 0;
-GLdouble radius = 1;
 
-void reshape(int x, int y) {
-    if (y == 0 || x == 0) return;
+GLint TIMER_DELAY = 200;
+GLfloat RED_RGB[] = { 1, 0, 0 };
+GLfloat BLUE_RGB[] = { 0, 0, 1 };
+GLfloat WHITE_RGB[] = { 1, 1, 1 };
+GLfloat BLACK_RGB[] = { 0, 0, 0 };
+GLfloat SUN_RGB[] = { 0.9, 0.3, 0.2 };
+int width = 500;
+int height = 500;
+GLfloat sun[1][3] = { 0.5, 0.5, 0.5 };
+float sunSize = 0.2;
+
+
+void myInit() {
+    glClearColor(0, 0, 0, 1);
+    glShadeModel(GL_SMOOTH);
+}
+
+void myReshape(int winWidth, int winHeight) {
+    width = winWidth;
+    height = winHeight;
+    glViewport(0, 0, winWidth, winHeight);
     glMatrixMode(GL_PROJECTION);
     glLoadIdentity();
-    GLdouble size = 1.0 * x / y;
-    gluPerspective(30, size, 3, 70);
+    gluOrtho2D(0.0, 1.0, 0.0, 1.0);
     glMatrixMode(GL_MODELVIEW);
-    glViewport(0, 0, x, y);
+    glutPostRedisplay();
+
 }
 
-void display(void) {
-
-    glMatrixMode(GL_MODELVIEW);
+void myWireSphere(float r, int nParal, int nMerid) {
+    float x, y, z, i, j;
     glClear(GL_COLOR_BUFFER_BIT);
-    glLoadIdentity();
+    glColor3fv(SUN_RGB);
+    for (j = 0; j < 3.14159; j += 3.14159 / (nParal + 1)) {
+        glBegin(GL_LINE_LOOP);
+        y = (float)(r * cos(j));
+        for (i = 0; i < 2 * 3.14159; i += 3.14159 / 60) {
+            x = (float)(r * cos(i) * sin(j));
+            z = (float)(r * sin(i) * sin(j));
+            glVertex3f(x + 0.8, y + 0.8, z);
+        }
+        glEnd();
+    }
 
-    glTranslatef(0, 0, -20);
-
-    glColor3f(0.9, 0.3, 0.2);
-    glRotatef(70, 1, 0, 0);
-    glRotatef(0, 0, 1, 0);
-    glRotatef(0, 0, 0, 1);
-    glScalef(1, 1, 1);
-    glutWireSphere(1, 20, 20);
-
-
-    glColor3f(0, 0, 1);
-    glRotatef(0, 1, 0, 0);
-    glRotatef(0, 0, 1, 0);
-    glRotatef(rotate, 0, 0, 1);
-    glScalef(1, 1, 1);
-    glTranslatef(5,1,0);
-    glutWireSphere(2, 20, 10);
-
-    glFlush();
+    for (j = 0; j < 3.14159; j += 3.14159 / nMerid) {
+        glBegin(GL_LINE_LOOP);
+        for (i = 0; i < 2 * 3.14159; i += 3.14159 / 60) {
+            x = (float)(r * sin(i) * cos(j));
+            y = (float)(r * cos(i + 0.5));
+            z = (float)(r * sin(j) * sin(i));
+            glVertex3f(x + 0.2, y + 0.2, z);
+        }
+        glEnd();
+    }
 }
 
-void idle(void) {
+void drawSun() {
 
-    rotate += 0.01;
-    display();
+    myWireSphere(0.2, 20, 20);
+}
+
+void myDisplay() {
+
+    drawSun();
+    glFlush();
+    glutSwapBuffers();
+}
+
+// Keyboard function.
+void keyboardFunct(unsigned char c, int x, int y) {
+
+    switch (c) {
+    case 'q':
+    case 'Q':
+        exit(0);
+        break;
+    case 'r':
+        break;
+    case 'a':
+        break;
+    default:
+        break;
+    }
+}
+
+// Catches arrow keys.
+void catchKeyFunct(int key, int x, int y) {
+
+    if (key == GLUT_KEY_LEFT) {
+
+    }
+    else if (key == GLUT_KEY_RIGHT) {
+
+    }
+    else if (key == GLUT_KEY_DOWN) {
+
+    }
+    else if (key == GLUT_KEY_UP) {
+
+    }
+}
+
+void mouseFunct(int b, int s, int x, int y) {
+
+    if (s == GLUT_DOWN) {
+        if (b == GLUT_LEFT_BUTTON) {
+
+        }
+        if (b == GLUT_RIGHT_BUTTON) {
+
+        }
+    }
+}
+
+void myTimeOut(int id) {
+
+    glutPostRedisplay();
+    glutTimerFunc(TIMER_DELAY, myTimeOut, 0);
 }
 
 int main(int argc, char** argv) {
-    glutInit(&argc, argv);
-    glutInitWindowSize(500, 500);
-    glutCreateWindow("Solid Sphere");
 
-    glutDisplayFunc(display);
-    glutReshapeFunc(reshape);
-    glutIdleFunc(idle);
+    glutInit(&argc, argv);
+    glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB);
+    glutInitWindowSize(width, height);
+    glutInitWindowPosition(0, 0);
+    glutCreateWindow("Problem 3");
+    glutDisplayFunc(myDisplay);
+    glutKeyboardFunc(keyboardFunct);
+    glutReshapeFunc(myReshape);
+    glutMouseFunc(mouseFunct);
+    glutTimerFunc(TIMER_DELAY, myTimeOut, 0);
+    myInit();
     glutMainLoop();
     return 0;
 }
