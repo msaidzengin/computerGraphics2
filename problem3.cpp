50c13a5f05f6e554747ab5d5d4749f7c1d98c372
msaidzengin <m.zengin@etu.edu.tr>
2020-02-15 19:58:14 +0300

    day rotate

--- problem3.cpp
+++ problem3.cpp
@@ -1,5 +1,6 @@
 # include <GL/glut.h>
 # include <math.h>
+#include <iostream>
 
 GLint TIMER_DELAY = 20;
 GLfloat BLUE_RGB[] = { 0, 0, 1 };
@@ -7,6 +8,7 @@ GLfloat SUN_RGB[] = { 0.9, 0.3, 0.2 };
 GLfloat sun[1][2] = { 0.5, 0.5 };
 float sunSize = 0.2;
 float yearDegree = 0;
+float dayDegree = 0;
 
 void myInit() {
     glClearColor(0, 0, 0, 1);
@@ -23,7 +25,7 @@ void myReshape(int winWidth, int winHeight) {
 
 }
 
-void myWireSphere(float r, int nParal, int nMerid, GLfloat color[], float xx, float yy) {
+void myWireSphere(float r, int nParal, int nMerid, GLfloat color[], float xx, float yy, int rotate) {
     float x, y, z, i, j;
     glColor3fv(color);
     for (j = 0; j < 3.14159; j += 3.14159 / (nParal + 1)) {
@@ -40,8 +42,8 @@ void myWireSphere(float r, int nParal, int nMerid, GLfloat color[], float xx, fl
     for (j = 0; j < 3.14159; j += 3.14159 / nMerid) {
         glBegin(GL_LINE_LOOP);
         for (i = 0; i < 2 * 3.14159; i += 3.14159 / 60) {
-            x = (float)(r * sin(i) * cos(j));
-            y = (float)(r * cos(i));
+            x = (float)(r * sin(i + dayDegree * rotate) * cos(j + dayDegree * rotate));
+            y = (float)(r * cos(i + dayDegree * rotate));
             z = (float)(r * sin(j) * sin(i));
             glVertex3f(x + xx, y + yy, z);
         }
@@ -57,16 +59,17 @@ void myDisplay() {
     float y = 0.12 * sin(yearDegree * 3.14159 / 180) + sun[0][1];
     float worldSize = -1 * 0.07 * sin(yearDegree * 3.14159 / 180) + 0.20;
     worldSize /= 2;
-    yearDegree += 1;
+    yearDegree += 0.5;
+    dayDegree += 0.01;
     if (yearDegree == 360) yearDegree = 1;
 
     if (y < 0.5) {
-        myWireSphere(sunSize, 20, 20, SUN_RGB, sun[0][0], sun[0][1]);
-        myWireSphere(worldSize, 20, 20, BLUE_RGB, x, y);
+        myWireSphere(sunSize, 20, 20, SUN_RGB, sun[0][0], sun[0][1], 0);
+        myWireSphere(worldSize, 20, 10, BLUE_RGB, x, y, 1);
     }
     else {
-        myWireSphere(worldSize, 20, 20, BLUE_RGB, x, y);
-        myWireSphere(sunSize, 20, 20, SUN_RGB, sun[0][0], sun[0][1]);
+        myWireSphere(worldSize, 20, 10, BLUE_RGB, x, y, 1);
+        myWireSphere(sunSize, 20, 20, SUN_RGB, sun[0][0], sun[0][1], 0);
     }
 
     glFlush();
